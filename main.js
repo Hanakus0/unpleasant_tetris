818fe8d4ea51b4cb3958f4da73b03ef129d4a429
Hanakus0 <everydoghas12day@outlook.jp>
2024-09-10 21:44:59 +0900

    part2

--- main.js
+++ main.js
@@ -2,12 +2,22 @@
 
 import {drawBlock} from './modules/parentModule.js';
 
-// パラメータ
+/** パラメータ*/
 const BLOCK_SIZE = 30 // ブロックサイズ(px)
 // const TETRO_SIZE = 4; // テトロミノの描画グリッド幅
-// canvasの取得および2d指定
+/** canvasの設定 */
+// テトリス領域 (20x10)
+const FIELD_COL = 10;
+const FIELD_ROW = 20;
+// キャンバスのサイズ
+const SCREEN_W = BLOCK_SIZE * FIELD_COL;
+const SCREEN_H = BLOCK_SIZE * FIELD_ROW;
+// 取得および2d指定
 let can = document.getElementById('can');
 let context = can.getContext('2d');
+// キャンバスのサイズ設定
+can.width = SCREEN_W
+can.height = SCREEN_H;
 
 
 // テトロミノのグリッド (ブロック部分 = １)
@@ -18,14 +28,60 @@ let tetro = [
   [0, 0, 0, 0]
 ];
 
-for(let y = 0; y < tetro.length; y++ ){
-  for(let x = 0; x < tetro[y].length; x++ ){
-    // 1 (ブロック部分) ならば描画
-    if(tetro[y][x]){
-      let px = x * BLOCK_SIZE;
-      let py = y * BLOCK_SIZE;
-      // ブロックの描画
-      drawBlock(context, px, py, BLOCK_SIZE);
+// テトロミノの座標
+let tetro_x = 0;
+let tetro_y = 0;
+
+/** テトロミノの描画 */
+drawTetro(); // 初期描画
+
+function drawTetro(){
+
+  context.clearRect(0, 0, SCREEN_W, SCREEN_H);
+
+  for(let y = 0; y < tetro.length; y++ ){
+    for(let x = 0; x < tetro[y].length; x++ ){
+      // 1 (ブロック部分) ならば描画
+      if(tetro[y][x]){
+        let px = (tetro_x + x) * BLOCK_SIZE;
+        let py = (tetro_y + y) * BLOCK_SIZE;
+        // ブロックの描画
+        drawBlock(context, px, py, BLOCK_SIZE);
+      }
     }
   }
+};
+
+
+// コントローラー
+// 十字キーでの操作
+document.onkeydown = function(e){
+  switch( e.key ){
+    // →
+    case "ArrowRight":
+      console.log("Right");
+      tetro_x++;
+      break;
+    // ↓
+    case "ArrowDown":
+      console.log("Down");
+      tetro_y++;
+      break;
+    // ←
+    case "ArrowLeft":
+      console.log("Left");
+      tetro_x--;
+      break;
+    // ↑
+    case "ArrowUp":
+      console.log("Up");
+      tetro_y--;
+      break;
+    // スペース
+    case " ":
+      console.log("␣");
+      break;
+  }
+  // コントローラーを押下するたびに描画
+  drawTetro();
 }
